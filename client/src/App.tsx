033ca64950c1c8c4a74d06e596face0fbe30aac6
chandan789maity <maity789chandan@gmail.com>
2024-08-05 22:08:05 +0530

    Ui fixed

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<Home/>} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`⚙️ Server is running at port: ${PORT}`);
+            console.log(` ⚙️  Server is running at port: ${PORT}`);
         });
     })
     .catch((err) => {
